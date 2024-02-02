e71c2237d801b214a952c95b9df8b661e2883ff4
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2024-02-03 02:30:27 +0530

    added a middleware

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+function middleware1(req, res, next) {
+
+    console.log("From inside middleware : " + req.headers.counter);
+    next();
+}
+app.use(middleware1);
+
 function calculateSum(counter) {
     var sum = 0;
     for (var i = 0; i <= counter; i += 1) {
@@ -11,14 +19,17 @@ function calculateSum(counter) {
 }
 
 function handleFirstRequest(req, res) {
-    var counter = req.query.counter;
+    // var counter = req.query.counter;
+    console.log(req.headers);
+    var counter = req.headers.counter;
     var calculatedSum = calculateSum(counter);
 
     var answer = "Sum is : " + calculatedSum;
     res.send(answer);
 }
 
-app.get('/handleSum', handleFirstRequest)
+// app.get('/handleSum', handleFirstRequest)
+app.post('/handleSum',handleFirstRequest)
 
 function started() {
     console.log(`Example app listening on port ${port}`)
